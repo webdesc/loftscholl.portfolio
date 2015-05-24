6c105b1992c9b1c7d7fac948aa38f4a9690da02c
Nunin Alexey <webdesc@gmail.com>
2015-05-24 21:50:38 +0300

    test commit

---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+var gulp = require('gulp'),
+	concatCss = require('gulp-concat-css'),
+	rename = require('gulp-rename'),
+	notify = require('gulp-notify'),
+	minifyCss = require('gulp-minify-css'),
+	autoprefixer = require('gulp-autoprefixer'),
+	livereload = require('gulp-livereload'),
+	connect = require('gulp-connect');
+
+// server connect
+gulp.task('connect', function() {
+  connect.server({
+    root: 'app',
+    livereload: true
+  });
+});
+
+// css
+gulp.task('css', function () {
+  gulp.src('css/*.css')
+    .pipe(concatCss('bundle.css'))
+    .pipe(autoprefixer('last 2 version', '> 1%', 'ie 9'))
+    .pipe(minifyCss())
+    .pipe(rename('bundle.min.css'))
+    .pipe(gulp.dest('app/css/'))
+    //.pipe(notify('Done!'))
+    .pipe(connect.reload());
+});
+
+// html
+gulp.task('html', function () {
+	gulp.src('app/index.html')
+	.pipe(connect.reload());
+});
+// watch
+gulp.task('watch', function () {
+	gulp.watch('css/*.css', ['css'])
+	gulp.watch('app/index.html', ['html'])
+});
+
+// default
+gulp.task('default', ['connect', 'html', 'css', 'watch']);
